b9dcb886dea64b37c81790842c3df07dc996a4f9
unknown <ChetanD@ChetanD-PC.(none)>
2012-07-09 20:59:53 +0530

    changed greeting

--- upload.php
+++ upload.php
@@ -2,7 +2,7 @@
 
 $target_path = "images/";
 
-$target_path = $target_path . basename( $_FILES['uploadedfile']['name']); 
+$target_path = $target_path.basename( $_FILES['uploadedfile']['name']); 
 
 if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
     echo "The file ".  basename( $_FILES['uploadedfile']['name']). 
