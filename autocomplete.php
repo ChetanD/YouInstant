14e6fe748496c43bb962fd024948c6146b84253f
unknown <ChetanD@ChetanD-PC.(none)>
2012-07-14 02:16:30 +0530

    changed greeting

--- autocomplete.php
+++ autocomplete.php
@@ -1,12 +1,14 @@
 <?php
    $url = "http://google.com/complete/search?output=toolbar&q=india";
-   		 $crl = curl_init();
-        $timeout = 5;
-        curl_setopt ($crl, CURLOPT_URL,$url);
-        curl_setopt ($crl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt ($crl, CURLOPT_CONNECTTIMEOUT, $timeout);
-        $ret = curl_exec($crl);
-        curl_close($crl);
-        echo $ret;
-		printf($ret)
+   		  $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$url);
+        curl_setopt($ch, CURLOPT_FAILONERROR,1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION,1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+        $retValue = curl_exec($ch);                      
+        curl_close($ch);
+       // return $retValue;
+        echo $retValue;
+		printf($retValue);
 ?>
