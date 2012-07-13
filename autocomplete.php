e7f3836c6197131483702a2599362ba905370fff
unknown <ChetanD@ChetanD-PC.(none)>
2012-07-14 03:23:01 +0530

    changed greeting

--- autocomplete.php
+++ autocomplete.php
@@ -21,8 +21,9 @@ curl_setopt($ch,CURLOPT_POST,count($fields));
 curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $response =json_encode(curl_exec ($curl));
+$geoloc = json_decode($response, true);
 curl_close ($curl);
-echo "demo:".$response;
+echo "demo:".$geoloc;
 $rxml = simplexml_load_string($response);
 echo $rxml;
 ?>
