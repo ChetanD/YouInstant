05f768a7bc5676c478ed682668e57fb02c459c7f
unknown <ChetanD@ChetanD-PC.(none)>
2012-07-14 02:51:58 +0530

    changed greeting

--- autocomplete.php
+++ autocomplete.php
@@ -25,7 +25,7 @@ $handle = fopen($daurl, "r");
 // If there is something, read and return
 if ($handle) {
     while (!feof($handle)) {
-        $buffer = fgets($handle->CompleteSuggestion, 4096);
+        $buffer = fgets($handle, 4096);
         echo $buffer;
     }
     fclose($handle);
