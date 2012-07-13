606cd67851de3f269491f1c8c5581a72520378e7
unknown <ChetanD@ChetanD-PC.(none)>
2012-07-14 03:13:37 +0530

    changed greeting

--- autocomplete.php
+++ autocomplete.php
@@ -19,6 +19,7 @@ $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL,$url);
 curl_setopt($ch,CURLOPT_POST,count($fields));
 curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $response = curl_exec ($curl);
 curl_close ($curl);
 echo "demo:".$response->title;
