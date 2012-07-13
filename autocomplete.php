674227418d2f50f001de11c60a8a2530ad2fbe96
unknown <ChetanD@ChetanD-PC.(none)>
2012-07-14 02:11:36 +0530

    changed greeting

---
+++ autocomplete.php
@@ -0,0 +1,11 @@
+<?php
+   $url = "http://google.com/complete/search?output=toolbar&q=india";
+   		 $crl = curl_init();
+        $timeout = 5;
+        curl_setopt ($crl, CURLOPT_URL,$url);
+        curl_setopt ($crl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt ($crl, CURLOPT_CONNECTTIMEOUT, $timeout);
+        $ret = curl_exec($crl);
+        curl_close($crl);
+        echo $ret;
+?>
