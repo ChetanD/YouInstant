0fc2576b557cf1c4475269deb5b21d97dd6eb4bc
unknown <ChetanD@ChetanD-PC.(none)>
2012-07-14 02:00:38 +0530

    changed greeting

--- api_call.php
+++ api_call.php
@@ -26,13 +26,14 @@ if (isset($_GET['tag'])) {
 
 # uses libcurl to return the response body of a GET request on $url
 function getResource($url){
-  $chandle = curl_init();
-  curl_setopt($chandle, CURLOPT_URL, $url);
-  curl_setopt($chandle, CURLOPT_RETURNTRANSFER, 1);
-  $result = curl_exec($chandle);
-  curl_close($chandle);
-  echo "demo".$result;
-  return $result;
+  		$ch = curl_init();
+		$timeout = 5;
+		curl_setopt($ch,CURLOPT_URL,$url);
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+		curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		return $data;
 }
 
 function do_search($tag) {
