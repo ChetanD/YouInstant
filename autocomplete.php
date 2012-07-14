ba43a9365de7086a3654015b9ba22e44573f1f78
unknown <ChetanD@ChetanD-PC.(none)>
2012-07-15 00:10:59 +0530

    changed greeting

--- autocomplete.php
+++ autocomplete.php
@@ -11,35 +11,7 @@ $geoloc = json_decode($response, true);
 curl_close ($curl);
 echo "demo:".$geoloc;
 $rxml = simplexml_load_string($response);
-echo $rxml;
- /* 
- * 
- 
-$url = 'http://api.flickr.com/services/xmlrpc/';
-$ch = curl_init($url);
- 
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
- 
-$response = curl_exec($ch);
-curl_close($ch);echo $response;
- * 
-
- $xml_data = 'foobar%';
-
-$URL = "http://www.example.com/api/foobar.xml";
-
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/html'));
-curl_setopt($ch, CURLOPT_POSTFIELDS, "$xml_data");
-$output = curl_exec($ch);
 $info = curl_getinfo($ch);
-curl_close($ch);
-
-print_r($info);
-print_r($output);
-*/
+echo $info;
+ 
 ?>
