6414b855f66dc847ca44ae9f4176c7ac8f105a33
unknown <ChetanD@ChetanD-PC.(none)>
2012-07-15 00:07:34 +0530

    changed greeting

--- autocomplete.php
+++ autocomplete.php
@@ -4,7 +4,7 @@ $ch = curl_init(); // initialize curl handle
 curl_setopt($ch, CURLOPT_VERBOSE, 1); // set url to post to 
 curl_setopt($ch, CURLOPT_URL, $url); // set url to post to 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // return into a variable 
-curl_setopt($ch, CURLOPT_HTTPHEADER, Array("Content-Type: text/xml")); 
+//curl_setopt($ch, CURLOPT_HTTPHEADER, Array("Content-Type: text/html")); 
 curl_setopt($ch, CURLOPT_HEADER, 1); 
 curl_setopt($ch, CURLOPT_TIMEOUT, 40); // times out after 4s 
 curl_setopt($ch, CURLOPT_POSTFIELDS, 'myxml='.urlencode($xml)); // 
