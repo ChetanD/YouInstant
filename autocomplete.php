bd6b530f4c8318c833f3d23dba6a69bc01f853b0
unknown <ChetanD@ChetanD-PC.(none)>
2012-07-15 00:46:08 +0530

    changed greeting

--- autocomplete.php
+++ autocomplete.php
@@ -14,10 +14,23 @@ echo "demo:".$response;
 $info = curl_getinfo($ch);
 echo $info;*
  * 
- */
+ 
 
  $response =file_get_contents($url);
  $response=var_dump($response);
  echo $response[0];
- 
+ */
+ $ch = curl_init('www.google.com');
+	curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+	curl_setopt($ch, CURLOPT_URL, $connection_url);
+	curl_setopt($ch, CURLOPT_COOKIE, $cookie);
+	curl_setopt($ch, CURLOPT_FAILONERROR, true);
+	curl_setopt($ch,CURLOPT_ENCODING, 1);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+	$data = curl_exec($ch);
+	return $data;
 ?>
