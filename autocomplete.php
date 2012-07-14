f80b6bd179e0e62da079728e4a10e0d1a17091a2
unknown <ChetanD@ChetanD-PC.(none)>
2012-07-15 00:05:22 +0530

    changed greeting

--- autocomplete.php
+++ autocomplete.php
@@ -1,16 +1,16 @@
 <?php
-   $url = "http://google.com/complete/search?output=toolbar&q=india";
-//$ch = curl_init(); // initialize curl handle 
-//curl_setopt($ch, CURLOPT_VERBOSE, 1); // set url to post to 
-//curl_setopt($ch, CURLOPT_URL, $url); // set url to post to 
-//curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // return into a variable 
-//curl_setopt($ch, CURLOPT_HTTPHEADER, Array("Content-Type: text/xml")); 
-//curl_setopt($ch, CURLOPT_HEADER, 1); 
-//curl_setopt($ch, CURLOPT_TIMEOUT, 40); // times out after 4s 
-//curl_setopt($ch, CURLOPT_POSTFIELDS, 'myxml='.urlencode($xml)); // 
-//curl_setopt($ch, CURLOPT_POST, 1); 
-//$result = curl_exec($ch); // run the whole process 
-//echo $result;
+  $url = "http://google.com/complete/search?output=toolbar&q=india";
+$ch = curl_init(); // initialize curl handle 
+curl_setopt($ch, CURLOPT_VERBOSE, 1); // set url to post to 
+curl_setopt($ch, CURLOPT_URL, $url); // set url to post to 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // return into a variable 
+curl_setopt($ch, CURLOPT_HTTPHEADER, Array("Content-Type: text/xml")); 
+curl_setopt($ch, CURLOPT_HEADER, 1); 
+curl_setopt($ch, CURLOPT_TIMEOUT, 40); // times out after 4s 
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'myxml='.urlencode($xml)); // 
+curl_setopt($ch, CURLOPT_POST, 1); 
+$result = curl_exec($ch); // run the whole process 
+echo $result;
 
 //$completeurl =
 //"http://ws.audioscrobbler.com/2.0/?method=&user=xgayax" .
@@ -38,11 +38,11 @@ curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
  
 $response = curl_exec($ch);
 curl_close($ch);echo $response;
- * */
+ * 
 
  $xml_data = 'foobar%';
 
-//$URL = "http://www.example.com/api/foobar.xml";
+$URL = "http://www.example.com/api/foobar.xml";
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_POST, 1);
@@ -55,5 +55,5 @@ curl_close($ch);
 
 print_r($info);
 print_r($output);
-
+*/
 ?>
