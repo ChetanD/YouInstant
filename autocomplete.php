4a47b545b3093cc265854d5fb8db2193353c0ae4
unknown <ChetanD@ChetanD-PC.(none)>
2012-07-15 00:08:38 +0530

    changed greeting

--- autocomplete.php
+++ autocomplete.php
@@ -1,20 +1,6 @@
 <?php
   $url = "http://google.com/complete/search?output=toolbar&q=india";
-$ch = curl_init(); // initialize curl handle 
-curl_setopt($ch, CURLOPT_VERBOSE, 1); // set url to post to 
-curl_setopt($ch, CURLOPT_URL, $url); // set url to post to 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // return into a variable 
-//curl_setopt($ch, CURLOPT_HTTPHEADER, Array("Content-Type: text/html")); 
-curl_setopt($ch, CURLOPT_HEADER, 1); 
-curl_setopt($ch, CURLOPT_TIMEOUT, 40); // times out after 4s 
-curl_setopt($ch, CURLOPT_POSTFIELDS, 'myxml='.urlencode($xml)); // 
-curl_setopt($ch, CURLOPT_POST, 1); 
-$result = curl_exec($ch); // run the whole process 
-echo $result;
 
-//$completeurl =
-//"http://ws.audioscrobbler.com/2.0/?method=&user=xgayax" .
-/*
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL,$url);
 curl_setopt($ch,CURLOPT_POST,count($fields));
@@ -25,8 +11,8 @@ $geoloc = json_decode($response, true);
 curl_close ($curl);
 echo "demo:".$geoloc;
 $rxml = simplexml_load_string($response);
-echo $rxml;*
- * 
+echo $rxml;
+ /* 
  * 
  
 $url = 'http://api.flickr.com/services/xmlrpc/';
