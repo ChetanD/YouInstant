fa8a5173fff6fb3fc283a734f737c52e1b74115f
unknown <ChetanD@ChetanD-PC.(none)>
2012-07-16 23:26:27 +0530

    changed greeting

---
+++ Dropbox/demo.php
@@ -0,0 +1,11 @@
+<?php
+    require 'dropboxoauth.php';
+	$consumer_key="6e6z7auri0u0rct";
+	$consumer_secret="kclsoe4oged57d7";
+	$callback_url="https://api.dropbox.com/1/oauth/request_token";
+	$oauth = new DropboxOAuth($consumer_key,$consumer_secret);
+
+	$request = $oauth->getRequestToken($callback_url);
+
+	$url = $oauth->getAuthorizeURL($request);
+?>
