c82825641e1fccf10e349d8d1cb309c3591c879c
unknown <ChetanD@ChetanD-PC.(none)>
2012-07-20 22:38:42 +0530

    changed greeting

--- yousearch.php
+++ yousearch.php
@@ -13,7 +13,7 @@ class seayoutube{
 		  
 		  //http://www.youtube.com/embed/dP15zlyra3c?html5=1
 		  $embededlink="http://www.youtube.com/embed/".$demo1[count($demo1)-1]."?autoplay=1&html5=1";
-		  $ret=$embededlink."~".$_REQUEST["count"];
+		  $ret=$embededlink;
 	      echo $ret;	
 		}
     }
