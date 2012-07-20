553efc02eb8a2a6b9b8b7ea3d81235e0b45bfef7
unknown <ChetanD@ChetanD-PC.(none)>
2012-07-20 21:47:15 +0530

    changed greeting

--- search.php
+++ search.php
@@ -18,10 +18,56 @@ if (isset($_REQUEST["q"]) && isset($_REQUEST["c"])) {
 	$term = trim(strtolower($_REQUEST['q']));
 	$url = str_replace(array(" ", "(", ")"), array("_", "", ""), $term);
 	require 'functions.php';
-	$youtube = new youtube();
-	$data = $youtube->get($url);
-	$arr = json_decode($data, true);
-	echo "[\"$url\",[";
+	switch ($_REQUEST["c"]) {
+		case 'g':
+			$google = new google();
+			$data = $google->get($url);
+			echo $data;
+			break;
+		case 'b':
+			$bing = new bing();
+			$data = $bing->get($url);
+			echo $data;
+			break;
+		case 'y':
+			$yahoo = new yahoo();
+			$data = $yahoo->get($url);
+			$str = substr($data, 0 , -9);
+			echo $str. "]";
+			break;
+		case 'w':
+			$wikipedia = new wikipedia();
+			$data = $wikipedia->get($url);
+			echo $data;
+			break;
+		case 'fbp':
+			$pages = new pages();
+			$data = $pages->get($url);
+			$arr = json_decode($data);
+			$count1 = json_decode($data, true);
+			$c = count($count1['data']);
+			$i = 1;
+			echo "[\"$url\",[";
+			foreach($arr->data as $item) {
+				echo "\"".$item->name."\"";
+					if ($i < $c) {
+						echo ",";
+					}
+					$i++;
+			}
+			echo "]]";
+			break;
+		case 'gb':
+			$books = new books();
+			$data = $books->get($url);
+			$i = 0;
+			var_dump(jsonp_decode($data, true));
+			break;
+		case 'yt':
+			$youtube = new youtube();
+			$data = $youtube->get($url);
+			$arr = json_decode($data, true);
+			echo "[\"$url\",[";
 			$c = count($arr[1]);
 			$i = 1;
 			foreach($arr[1] as $item) {
@@ -33,7 +79,57 @@ if (isset($_REQUEST["q"]) && isset($_REQUEST["c"])) {
 			}
 			echo "]]";
 			break;
-	
+		case 'gm':
+			$gmaps = new gmaps();
+			$data = $gmaps->get($url);
+			$arr = json_decode($data);
+			$i = 1;
+			try{
+			echo "[\"$url\",[";
+				$c = count($arr->suggestion);
+				if (empty($c)) {
+					throw new Exception("[\"error occured\"]");
+				}
+			foreach($arr->suggestion as $item) {
+				echo "\"".$item->query."\"";
+				if ($i < $c) {
+					echo ",";
+				}
+				$i++;
+			}
+			} catch (Exception $e) {
+				die($e->getMessage());
+			}
+			echo "]]";
+			break;
+		case 'i':
+			$imdb = new imdb();
+			$data = $imdb->process($url);
+			preg_match('/^imdb\$.*?\((.*?)\)$/ms', $data, $matches); //convert JSONP to JSON
+			$json = $matches[1];
+			$arr = json_decode($json);
+			echo "[\"$url\",[";
+			$c = count($arr->d);
+			$i = 1;
+			try {
+				if (!$c) {
+					throw new Exception("error occured");
+				}
+				foreach($arr->d as $item) {
+					echo "\"".$item->l."\"";
+					if ($i < $c) {
+						echo ",";
+						$i++;
+					}
+				}
+			} catch (Exception $e) {
+				die($e->getMessage());
+			}
+			echo "]]";
+			break;
+		default:
+			print "c is undefined!";
+	}
 
 } else {
 	$str = "not found";
