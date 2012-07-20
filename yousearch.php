bdc2e5cfd542eee5928f0fef15748c01609102ab
unknown <ChetanD@ChetanD-PC.(none)>
2012-07-21 00:21:11 +0530

    changed greeting

--- yousearch.php
+++ yousearch.php
@@ -3,21 +3,26 @@
 class seayoutube{
 	public function get($q){
 		$q=str_replace(" ", "/", $q);
-	   $feedURL = "http://gdata.youtube.com/feeds/api/videos/-/".$q."?orderby=viewCount&max-results=1";
+	   $feedURL = "http://gdata.youtube.com/feeds/api/videos/-/".$q."?orderby=viewCount&max-results=5";
 	   $sxml = simplexml_load_file($feedURL);
+	   $ret="";
 	   foreach ($sxml->entry as $entry) {
 		  $id=$entry->id;
 		  $demo=(string)$id;
 		  $demo1=explode("/", $demo);
 		  
-		  
+	    	  
 		  //http://www.youtube.com/embed/dP15zlyra3c?html5=1
 		  $embededlink="http://www.youtube.com/embed/".$demo1[count($demo1)-1]."?autoplay=1&html5=1";
 		  $media = $entry->children('http://search.yahoo.com/mrss/');
-		  $ret=$embededlink.",".$media->group->title;
-	      return $ret;	
+		  $attrs = $media->group->thumbnail[0]->attributes();
+      	  $thumbnail = $attrs['url'];
+		  $ret.=$embededlink."&".$media->group->title."&".$thumbnail."&";	
 		}
+	   
+	   return $ret;
     }
+	
 }
     
 ?>
