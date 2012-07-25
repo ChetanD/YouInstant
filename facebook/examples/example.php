9a58a4c9415ac7236158c62db394998e7c0c84fa
Chetan Dhembre <chetandhembre@gmail.com>
2012-07-25 23:31:39 +0530

    changed greeting

--- facebook/examples/example.php
+++ facebook/examples/example.php
@@ -102,6 +102,7 @@ $naitik = $facebook->api('/naitik');
 <script type="text/javascript">
   	function query(event,that){
   	//	var event=window.event;
+  	alert(event.keyCode);
   		if(event.keycode==13){
   			alert(that.value);
   		}
