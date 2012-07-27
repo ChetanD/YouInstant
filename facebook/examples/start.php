86e7cbf2145768439cbd6b1e51639325c9ebbfdb
Chetan Dhembre <chetandhembre@gmail.com>
2012-07-28 00:12:01 +0530

    changed greeting

--- facebook/examples/start.php
+++ facebook/examples/start.php
@@ -1,16 +1,8 @@
 <?php
 	require "login.php";
 	session_start();  
-	if(isset($_SESSION['user'])){
-		echo $return12;
-	}
-	else{
-		$login=new fblogin();
-		$return12 = $login->login();
-		$_SESSION['user']=$return12;
-		echo $return12;
-	}
-	
+    $fblogin=new fblogin();
+	return $fblogin->login();  
     
     
 ?>
