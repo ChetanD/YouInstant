3d88ddd86aeaef68cc7ead9af12ed383d490ebde
ChetanD <chetandhembre@gmail.com>
2012-08-22 11:25:54 +0530

    changed code

--- facebook/examples/start.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require "login.php";
-	//session_start();  
-    $fblogin=new fblogin();
-	$r=$fblogin->login();
-	return $r;  
-    
-    
-?>
--- gitme.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-git add .
-git commit -am "changed greeting"
-git push heroku master
-if %ERRORLEVEL% EQU 0 echo OK
-
-  
